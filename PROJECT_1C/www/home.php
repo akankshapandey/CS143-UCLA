9b7a534fcef673d6db7c948d9dfebdbae8aa05a9
akankshapandey <akankshapandey.162@gmail.com>
2016-01-12 06:23:24 +0530

    Create home.php

---
+++ PROJECT_1C/www/home.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<title>
+			Home
+		</title>
+		<body bgcolor="pink">
+			<center>
+				<table border="0" width="900">
+					<tr>
+						<th BGCOLOR="Gainsboro">
+							<a href="addA-D.php">Add Actor or Director</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="addMovieInfo.php">Add Movie Info</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="addReview.php">Add Movie Review</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="addMovieActor.php">Add Actor To movie</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="addMovieDirector.php">Add Director To Movie</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="browseActor.php">Browse Actor</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="browseMovie.php">Browse Movie</a>
+						</th>
+						<th>
+						</th>
+						<th BGCOLOR="Gainsboro">
+							<a href="search.php">Search</a>
+						</th>
+						<th>
+						</th>
+					</tr>
+				</table>
+				<br>
+				<br>
+				<br>
+				<h4>
+					Hi! Welcome to Project 1C!
+				</h4>
+			</body>
+		</center>
+	</html>
